8743c93e97f6036b4483e98cbd47634c43bf0dea
vladplesu <vladp1987@gmail.com>
2019-05-24 23:56:44 +0300

    finised adding webpack

--- src/index.js
+++ src/index.js
@@ -1,1 +1,2 @@
 // Main app
+import './styles/main.scss';
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,16 @@
 const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   module: {
     rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
       {
         test: /\.html$/,
         use: [
@@ -11,6 +19,14 @@ module.exports = {
             options: { minimize: true }
           }
         ]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ['file-loader']
+      },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
       }
     ]
   },
@@ -18,6 +34,24 @@ module.exports = {
     new HtmlWebPackPlugin({
       template: './src/index.html',
       filename: './index.html'
+    }),
+    new HtmlWebPackPlugin({
+      template: './src/pages/recover.html',
+      filename: './recover/index.html'
+    }),
+    new HtmlWebPackPlugin({
+      template: './src/pages/new-user.html',
+      filename: './new-user/index.html'
+    }),
+    new HtmlWebPackPlugin({
+      template: './src/pages/dashboard.html',
+      filename: './dashboard/index.html'
+    }),
+    new MiniCssExtractPlugin({
+      // Options similar to the same option in webpackOptions.output
+      // both options are optional
+      filename: '[name].css',
+      chunkFilename: '[id].css'
     })
   ]
 };
